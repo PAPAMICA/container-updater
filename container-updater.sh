509397388ed7acac2ec719047d32f8bbcd0fd0e6
PAPAMICA <mickael@papamica.com>
2022-04-29 20:26:28 +0200

    first release

---
+++ container-updater.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+DISCORD_WEBHOOK=$1
+for CONTAINER in $(docker ps --format {{.Names}}); do
+    IMAGE=$(docker container inspect $CONTAINER | jq -r '.[].Config.Image' | cut -d: -f1)
+    token=$(curl --silent "https://auth.docker.io/token?scope=repository:$IMAGE:pull&service=registry.docker.io" | jq -r '.token')
+    digest=$(curl --silent -H "Accept: application/vnd.docker.distribution.manifest.v2+json" \
+        -H "Authorization: Bearer $token" \
+        "https://registry.hub.docker.com/v2/$IMAGE/manifests/latest" | jq -r '.config.digest')
+    local_digest=$(docker images -q --no-trunc $IMAGE)
+    if [ "$digest" != "$local_digest" ] ; then
+        UPDATE=$(echo -E "$UPDATE$IMAGE\n")
+        CONTAINERS=$(echo -E "$CONTAINERS$CONTAINER\n")
+    fi
+    AUTOUPDATE=""
+    AUTOUPDATE=$(docker container inspect $CONTAINER | jq -r '.[].Config.Labels."autoupdate"')
+    PORTAINER_WEBHOOK=$(docker container inspect $CONTAINER | jq -r '.[].Config.Labels."autoupdate.webhook"')
+    if [ "$AUTOUPDATE" == "true" ]; then
+        echo "$CONTAINER A METTRE A JOUR ($PORTAINER_WEBHOOK)"
+        UPDATED=$(echo -E "$UPDATED$CONTAINER\n")
+    fi
+    docker image prune -f
+done
+
+curl  -H "Content-Type: application/json" \
+  -d '{
+    "content": null,
+    "embeds": [
+      {
+        "title": "There are some updates to do !",
+        "color": 16759896,
+        "fields": [
+          {
+            "name": "Container",
+            "value": "'$CONTAINERS'",
+            "inline": true
+          },
+          {
+            "name": "Images",
+            "value": "'$UPDATE'",
+            "inline": true
+          },
+          {
+            "name": "Auto Updated",
+            "value": "'$UPDATED'",
+            "inline": false
+          }
+        ],
+        "author": {
+          "name": "'$HOST'"
+        }
+      }
+    ],
+    "attachments": []
+  }' \
+  $DISCORD_WEBHOOK
